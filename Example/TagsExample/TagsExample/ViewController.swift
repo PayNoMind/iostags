acbf6120ef886b884b41d545f0935335b7d7c67b
Tom <tom@fluid-dynamics.ca>
2015-10-16 10:54:50 +0200

    Moving things around.

---
+++ Example/TagsExample/TagsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TagsExample
+//
+//  Created by Tom Clark on 2015-10-16.
+//  Copyright © 2015 Fluiddynamics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ tags/Tags.h
@@ -0,0 +1,19 @@
+//
+//  Tags.h
+//  Tags
+//
+//  Created by Tom Clark on 2015-10-16.
+//  Copyright © 2015 Fluiddynamics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tags.
+FOUNDATION_EXPORT double TagsVersionNumber;
+
+//! Project version string for Tags.
+FOUNDATION_EXPORT const unsigned char TagsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tags/PublicHeader.h>
+
+
